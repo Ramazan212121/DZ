745d27cb37a29e8cc3017adbf0d4d865af441704
Ramazan212121 <56290333+Ramazan212121@users.noreply.github.com>
2019-12-07 18:52:10 +0300

    Create adminpanel.php

---
+++ adminpanel.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="adminpanel.css">
+</head>
+<body>
+	<a href="reviews.php">
+	<div class="suggestions" href="/suggestions.php">
+		<h2 style="align:center">Предложения от пользователей</h2>
+	</div></a>
+	<a href="/addmark.php">
+	<div class="newmarks">
+		<h2 style="align:center">Добавление новой марки</h2>
+	</div>
+	</a>
+		<a href="/addnews.php">
+	<div class="addnews">
+		<h2 style="text-align:center">Добавление новостей</h2>
+	</div>
+	</a>
+	<a href="/main.php">
+	<div class="back">
+		<h2 style="text-align:center" class="s1">На главную</h2>
+	</div>
+	</a>
+	
+</body>
+</html>
