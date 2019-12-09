431452c3cdc580ff3471e2bab842181719563171
Ramazan212121 <56290333+Ramazan212121@users.noreply.github.com>
2019-12-09 17:54:54 +0300

    Create addmarksscr.php

---
+++ addmarksscr.php
@@ -0,0 +1,20 @@
+<?php 
+	$mark = filter_var(trim($_POST['mark']),
+	FILTER_SANITIZE_STRING);
+	$start = filter_var(trim($_POST['start']),
+	FILTER_SANITIZE_STRING);
+	$text = filter_var(trim($_POST['text']),
+	FILTER_SANITIZE_STRING);
+	$photo = addslashes(file_get_contents($_FILES['photo']['tmp_name']));
+	$mysql=new mysqli('localhost','root','','site');
+		$mysql->query("INSERT INTO `marks` (`mark`,`start`,`text`,`photo`)
+		VALUES('$mark','$start','$text','$photo')");
+		$mysql->query("INSERT INTO `justmarks` (`mark`)
+		VALUES('$mark')");
+
+	
+
+	$mysql->close;
+	header('Location:/adminpanel.php');
+  ?>
+	
